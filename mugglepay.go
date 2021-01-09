c117a6e48bc9871e6cb41953ae2920089f6797a4
干净 <54979375+du5@users.noreply.github.com>
2021-01-09 21:21:01 +0800

    init: VerifyOrder & GetOrder

--- mugglepay.go
+++ mugglepay.go
@@ -68,20 +68,45 @@ type Invoice struct {
 	QrcodeLg        string  `json:"qrcodeLg"`
 }
 
-type SOrder struct {
+type ServerOrder struct {
 	Status     int     `json:"status"`
 	Order      Order   `json:"order"`
 	PaymentUrl string  `json:"payment_url"`
 	Invoice    Invoice `json:"invoice"`
 }
 
-func (mgp *Mugglepay) CreateOrder(order *Order) (SOrder, error) {
+type Callback struct {
+	MerchantOrderId string  `json:"merchant_order_id"`
+	OrderId         string  `json:"order_id"`
+	Status          string  `json:"status"`
+	PriceAmount     float64 `json:"price_amount"`
+	PriceCurrency   string  `json:"price_currency"`
+	PayAmount       float64 `json:"pay_amount"`
+	PayCurrency     string  `json:"pay_currency"`
+	CreatedAt       string  `json:"created_at"`
+	CreatedAtT      int64   `json:"created_at_t"`
+	Token           string  `json:"token"`
+	Meta            Meta    `json:"meta"`
+}
+
+type Meta struct {
+	Payment     string  `json:"payment"`
+	TotalMmount float64 `json:"total_amount"`
+	TradeNo     string  `json:"trade_no"`
+	OutTradeNo  string  `json:"out_trade_no"`
+}
+
+func (mgp *Mugglepay) CreateOrder(order *Order) (ServerOrder, error) {
 	mgp.ApiUrl = mgp.ApiUrl + "/orders"
 
-	var sorder SOrder
-	if order.MerchantOrderId == "" {
-		return sorder, errors.New("merchant_order_id cannot be null")
+	var sorder ServerOrder
+	if mgp.ApplicationKey == "" {
+		return sorder, errors.New("application key cannot be null")
 	}
+	if order.MerchantOrderId == "" {
+		return sorder, errors.New("merchant order id cannot be null")
+	}
+
 	if order.PriceCurrency == "" {
 		order.PriceCurrency = "CNY"
 	}
@@ -98,19 +123,16 @@ func (mgp *Mugglepay) CreateOrder(order *Order) (SOrder, error) {
 		order.SuccessUrl = mgp.SuccessUrl + order.MerchantOrderId
 	}
 
+	// 签名
 	order.Sign(mgp.ApplicationKey)
 
 	jsonOrder, _ := json.Marshal(order)
-	client := &http.Client{}
+
 	reqest, _ := http.NewRequest("POST", mgp.ApiUrl, bytes.NewBuffer(jsonOrder))
 	reqest.Header.Add("token", mgp.ApplicationKey)
 	reqest.Header.Add("content-type", "application/json")
-	response, _ := client.Do(reqest)
-	responseB := response.Body
-	defer responseB.Close()
-	body, _ := ioutil.ReadAll(responseB)
-	bytes := []byte(body)
-	_ = json.Unmarshal(bytes, &sorder)
+	http_unmarshal(reqest, &sorder)
+
 	return sorder, nil
 }
 
@@ -120,4 +142,43 @@ func (order *Order) Sign(secret string) {
 	q.Set("secret", secret)
 	q.Set("type", "FIAT")
 	order.Token = strings.ToLower(fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%x", md5.Sum([]byte(q.Encode())))+secret))))
+}
+
+// 校验订单 true: 已支付; false: 未支付/取消/欺诈
+func (mgp *Mugglepay) VerifyOrder(callback *Callback) bool {
+	if mgp.ApplicationKey == "" {
+		return false
+	}
+	order := &Order{MerchantOrderId: callback.MerchantOrderId}
+	order.Sign(mgp.ApplicationKey)
+	// 校验签名
+	if order.Token != callback.Token {
+		return false
+	}
+	if callback.Status == "PAID" {
+		return true
+	}
+	return false
+}
+
+func (mgp *Mugglepay) GetOrder(OrderId string) (ServerOrder, error) {
+	var sorder ServerOrder
+	if OrderId == "" {
+		return sorder, errors.New("order id cannot be null")
+	}
+	mgp.ApiUrl = mgp.ApiUrl + "/orders/" + OrderId
+	reqest, _ := http.NewRequest("GET", mgp.ApiUrl, nil)
+	reqest.Header.Add("token", mgp.ApplicationKey)
+	http_unmarshal(reqest, &sorder)
+	return sorder, nil
+}
+
+func http_unmarshal(reqest *http.Request, sorder *ServerOrder) {
+	client := &http.Client{}
+	response, _ := client.Do(reqest)
+	responseB := response.Body
+	defer responseB.Close()
+	body, _ := ioutil.ReadAll(responseB)
+	bytes := []byte(body)
+	_ = json.Unmarshal(bytes, &sorder)
 }
